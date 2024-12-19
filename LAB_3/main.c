8fc65173e4b5a52484eb146c022c4dd69ab4b866
ariel-j <arieljay@post.bgu.ac.il>
2024-12-19 14:44:47 +0200

    completed task 0: basic aseemblly, linker and make

--- LAB_3/main.c
+++ LAB_3/main.c
@@ -1,18 +1,18 @@
 #include "Util.h"
 
-#define SYS_WRITE 4
 #define STDOUT 1
-#define SYS_OPEN 5
-#define O_RDWR 2
-#define SYS_SEEK 19
-#define SEEK_SET 0
-#define SHIRA_OFFSET 0x291
+#define SYS_WRITE 4
+#define STDERR 2
 
 extern int system_call();
 
-int main (int argc , char* argv[], char* envp[])
-{
-  /*Complete the task here*/
-
-  return 0;
+int main (int argc, char* argv[], char* envp[]) {
+    int i;
+    
+    for(i = 0; i < argc; i++) {
+        system_call(SYS_WRITE, STDOUT, argv[i], strlen(argv[i]));
+        system_call(SYS_WRITE, STDOUT, "\n", 1);
+    }
+    
+    return 0;
 }
