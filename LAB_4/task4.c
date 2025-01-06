e2f2d7707507e94e0b48452a69999b19c2dba873
ariel-j <arieljay@post.bgu.ac.il>
2025-01-06 21:58:55 +0200

    FINSIH

---
+++ LAB_4/task4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int count_digits(char* str) {
+   int count = 0;
+   while(*str) {
+       if(*str >= '0' && *str <= '9') count++;
+       str++;
+   }
+   return count;
+}
+
+int main(int argc, char** argv) {
+    if(argc != 2) {
+        printf("Usage: %s <string>\n", argv[0]);
+        return 1;
+    }
+    
+    printf("String contains %d digits, right???\n", count_digits(argv[1]));
+    return 0;
+}
